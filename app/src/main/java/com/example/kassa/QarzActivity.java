6a6aa8ace9645ec823e961143dd79aae7e1399c2
asadbek <AsadbekTurgunboyev>
2022-02-24 11:14:04 +0500

    as

--- app/src/main/java/com/example/kassa/QarzActivity.java
+++ app/src/main/java/com/example/kassa/QarzActivity.java
@@ -2,29 +2,41 @@ package com.example.kassa;
 
 import static com.example.kassa.ShowActivity.showModelList;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.Editable;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.MultiAutoCompleteTextView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
-public class QarzActivity extends AppCompatActivity {
+public class QarzActivity extends AppCompatActivity implements View.OnClickListener {
     AutoCompleteTextView ism;
-    EditText familya, telefon,izoh;
+    EditText familya, telefon, izoh;
     MaterialButton btn_qarz;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    List<QarzModel> qarzList;
+    List<String> nameQarz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +44,20 @@ public class QarzActivity extends AppCompatActivity {
         setContentView(R.layout.activity_qarz);
         //get current date time with Date()
         initViews();
-        String[] cities =  { "Paries,France", "PA,United States","Parana,Brazil",
-                "Padua,Italy", "Pasadena,CA,United States"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,cities);
-        ism.setThreshold(1);
-        ism.setAdapter(adapter);
-
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("qarzlar");
+        btn_qarz.setOnClickListener(this);
+//        String[] cities =  { "Paries,France", "PA,United States","Parana,Brazil",
+//                "Padua,Italy", "Pasadena,CA,United States"};
+//
     }
 
     private void initViews() {
         ism = (AutoCompleteTextView) findViewById(R.id.editText);
         familya = findViewById(R.id.editText2);
         telefon = findViewById(R.id.editText3);
+        btn_qarz = findViewById(R.id.btn_qarz);
+        izoh = findViewById(R.id.note);
 
     }
 
@@ -85,7 +99,8 @@ public class QarzActivity extends AppCompatActivity {
         int space = 8 - str.length();
         return str + new String(new char[space]).replace("\0", "\t");
     }
-    private void set(){
+
+    private void set() {
         Date date = new Date();
         @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         @SuppressLint("SimpleDateFormat") DateFormat timeFormat = new SimpleDateFormat("HH:mm");
@@ -128,5 +143,74 @@ public class QarzActivity extends AppCompatActivity {
         bill.append(amt);
 //        textView.setText(bill.toString());
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        String name = ism.getText().toString();
+        String fam = familya.getText().toString();
+        String number = telefon.getText().toString();
+        String note = izoh.getText().toString();
+        if (name.isEmpty() || fam.isEmpty() || number.isEmpty()) {
+            Toast.makeText(this, "Bo'sh joylarni to'ldiring", Toast.LENGTH_SHORT).show();
+        } else {
+            String key = String.valueOf(reference.push().getKey());
+            QarzModel model = new QarzModel(name + " " + fam, number, note, key, 0);
+            reference.child(Objects.requireNonNull(key)).setValue(model).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(QarzActivity.this, "Qarzdor ro'yhatdan o'tkazildi!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        qarzList = new ArrayList<>();
+        nameQarz = new ArrayList<>();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                qarzList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    QarzModel model = ds.getValue(QarzModel.class);
+                    qarzList.add(model);
+                }
+                nameQarz.clear();
+                for (int i = 0; i < qarzList.size(); i++) {
+                    nameQarz.add(qarzList.get(i).getName());
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.select_dialog_item, nameQarz);
+                ism.setThreshold(1);
+                ism.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        getData( qarzList.get((Integer) adapterView.getItemAtPosition(i)));
+
+                    }
+                });
+                ism.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+    private void getData(QarzModel model) {
+        String[] a = model.getName().split(" ");
+        String nam = String.valueOf(a[0]);
+        String fam = String.valueOf(a[1]);
+        ism.setText(nam);
+        familya.setText(fam);
+        telefon.setText(model.getNumber());
+        izoh.setText(model.getNote());
     }
 }
